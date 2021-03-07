f3097115d8382016ff1283ea227082bc37a596d9
Ddteam99 <deisi1999@gmail.com>
2021-03-07 14:03:25 +0200

    Scripted the menu and added the return to the menu button on the credits
    Scripted the main menu (Outside of settings button, to which there is still no scene). Also added the return to the menu button on the credits.

---
+++ Assets/ReturntotheMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturntotheMenu : MonoBehaviour {
+    
+    public void Return ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
