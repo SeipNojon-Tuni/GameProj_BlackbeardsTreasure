e28920b3edbf9a07967da2de49e57befb96208f1
Ddteam99 <deisi1999@gmail.com>
2021-03-15 15:51:01 +0200

    Added score counter and return to the menu button on the game. Removed the settings button from the main menu and also created the Coroutine script to get the set sail sound play when pressing set sail button (I was not successful)
    Added score counter and return to the menu button on the game. Removed the settings button from the main menu and also created the Coroutine script to get the set sail sound play when pressing set sail button (I was not successful). I already added two game objects to the main menu scene: Audio which has the sound file (It is already connected to the set sail button on click component.)
    Timer has Coroutine script connected to it, but it is not yet connected to any buttons

---
+++ Assets/Coroutine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Test());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Test()
+    {
+        yield return new WaitForSeconds(4);
+        Debug.Log("Wait is over");
+    }
+        
+}
